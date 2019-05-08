f074d378502901e3582559f6d9841a17a5007367
Sim Wang <sim.wang@oracle.com>
2019-05-08 16:48:39 +0800

    https://leetcode.com/problems/reverse-integer

---
+++ leetcode/src/main/java/exercise/leetcode/ora/ReverseInteger.java
@@ -0,0 +1,20 @@
+package exercise.leetcode.ora;
+
+public class ReverseInteger {
+    public int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            if ((res > Integer.MAX_VALUE / 10 && res * 10 < Integer.MAX_VALUE - x % 10)
+                    || (res < Integer.MIN_VALUE / 10 && res * 10 > Integer.MIN_VALUE - x % 10)) {
+                return 0;
+            }
+            res = res * 10 + x % 10;
+            x = x / 10;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ReverseInteger().reverse(1463847412));
+    }
+}
