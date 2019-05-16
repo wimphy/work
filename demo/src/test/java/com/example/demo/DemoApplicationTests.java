e5d8828257eab44489bdb2240037880ebb06cf23
Sim Wang <sim.wang@oracle.com>
2019-05-16 15:49:35 +0800

    spring boot demo

---
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static java.util.Calendar.FRIDAY;
+import static java.util.Calendar.MONDAY;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
