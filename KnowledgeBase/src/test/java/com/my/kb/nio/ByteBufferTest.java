d37e20bfc07da901eac29c78b1922383dfb33616
Sim Wang <sim.wang@oracle.com>
2019-10-12 16:12:32 +0800

    nio vs bio ByteBuffer test

---
+++ KnowledgeBase/src/test/java/com/my/kb/nio/ByteBufferTest.java
@@ -0,0 +1,65 @@
+package com.my.kb.nio;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+
+import static com.my.kb.utils.MyLogger.log;
+
+public class ByteBufferTest {
+    @Test
+    public void test() throws Exception {
+        String s = "abc";//3 bytes data
+        Assert.assertEquals(3, s.getBytes().length);
+
+        ByteBuffer buffer = ByteBuffer.allocate(5);//initial pointers
+        Assert.assertEquals(5, buffer.limit());
+        Assert.assertEquals(5, buffer.capacity());
+        Assert.assertEquals(0, buffer.position());
+
+        buffer.put(s.getBytes());//write 3 bytes data
+        Assert.assertEquals(5, buffer.limit());
+        Assert.assertEquals(5, buffer.capacity());
+        Assert.assertEquals(3, buffer.position());
+
+        buffer.flip();//set position to 0
+        Assert.assertEquals(3, buffer.limit());
+        Assert.assertEquals(5, buffer.capacity());
+        Assert.assertEquals(0, buffer.position());
+
+        byte b = buffer.get();//get 1 byte data, and set current pos
+        Assert.assertEquals('a', b);
+        Assert.assertEquals(3, buffer.limit());
+        Assert.assertEquals(5, buffer.capacity());
+        Assert.assertEquals(1, buffer.position());
+
+        buffer.rewind();//set current pos to 0
+        Assert.assertEquals(3, buffer.limit());
+        Assert.assertEquals(5, buffer.capacity());
+        Assert.assertEquals(0, buffer.position());
+
+        buffer.get();
+        buffer.get();
+        buffer.mark();//mark current pos
+        Assert.assertEquals(3, buffer.limit());
+        Assert.assertEquals(5, buffer.capacity());
+        Assert.assertEquals(2, buffer.position());
+
+        buffer.get();//get 1 more byte
+        Assert.assertEquals(3, buffer.limit());
+        Assert.assertEquals(5, buffer.capacity());
+        Assert.assertEquals(3, buffer.position());
+
+        buffer.reset();//reset current pos to marked pos
+        Assert.assertEquals(3, buffer.limit());
+        Assert.assertEquals(5, buffer.capacity());
+        Assert.assertEquals(2, buffer.position());
+
+        buffer.clear();//clear all data, which equals the initial state
+        Assert.assertEquals(5, buffer.limit());
+        Assert.assertEquals(5, buffer.capacity());
+        Assert.assertEquals(0, buffer.position());
+    }
+
+}
