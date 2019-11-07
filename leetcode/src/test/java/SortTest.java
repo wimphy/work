f7bab8f80e4c7cb3f80148635e96a15de696abeb
Sim Wang <sim.wang@oracle.com>
2019-11-07 09:10:57 +0800

    heap sort

--- leetcode/src/test/java/SortTest.java
+++ leetcode/src/test/java/SortTest.java
@@ -1,8 +1,6 @@
+import org.junit.Assert;
 import org.junit.Test;
-import sorting.Insertion;
-import sorting.Merge;
-import sorting.Selection;
-import sorting.Sort;
+import sorting.*;
 
 import java.util.Arrays;
 import java.util.Random;
@@ -104,5 +102,46 @@ public class SortTest {
         sort.sort2(arr);
         log("time(ms): " + (System.currentTimeMillis() - start));
         log(arr); //00:00.293
+    }
+
+    @Test
+    public void HeapTest() {
+        Sort sort = new Heap();
+        int[] arr = {1,9, 4, 20, 23,15,14,99,2};
+        log(Arrays.toString(arr));
+        sort.sort(arr);
+        log(Arrays.toString(arr));
+    }
+
+    @Test
+    public void HeapTestBig() {
+        Sort sort = new Heap();
+        int count = 1000000;
+        int[] arr = new int[count];
+        Random r = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = r.nextInt(count);
+        }
+        log(arr);
+        long start = System.currentTimeMillis();
+        sort.sort(arr);
+        log("time(ms): " + (System.currentTimeMillis() - start));
+        log(arr); //00:00.499
+        assertInOrder(arr);
+    }
+
+    //small to big
+    private void assertInOrder(int[] arr) {
+        Assert.assertNotNull(arr);
+        Assert.assertTrue(arr.length > 0);
+        int[] x = {0, arr[0]};
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < x[1]) {
+                log(String.format("[%d]:{%d} > [{%d}]:{%d}", x[0], x[1], i, arr[i]));
+                Assert.fail();
+            }
+            x[0] = i;
+            x[1] = arr[i];
+        }
     }
 }
