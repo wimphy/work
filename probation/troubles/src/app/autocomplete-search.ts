1c319613c47b2b4d5f657ed0cb2e75263e1e6bdd
Simon Wang <wimphy@126.com>
2020-10-10 08:58:25 +0800

    before router

--- probation/troubles/src/app/autocomplete-search.ts
+++ probation/troubles/src/app/autocomplete-search.ts
@@ -3,6 +3,7 @@ import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { HttpClient } from '@angular/common/http';
 
 /**
  * @title Highlight the first autocomplete option
@@ -17,22 +18,38 @@ export class AutocompleteSearch implements OnInit {
   options: string[] = ['One', 'Two', 'Three'];
   filteredOptions: Observable<string[]>;
   searchContent: string
+  name: string
 
-  ngOnInit() {
+  constructor(private http: HttpClient) { }
+
+  private search(): void {
+    if(this.searchContent === undefined){
+      this.searchContent = '';
+    }
+    const filterValue = this.searchContent.toLowerCase();
+    let res = this.options.filter(option => option.toLowerCase().indexOf(filterValue) != -1);
+    if(res.length === 0){
+      res = [this.searchContent];
+    }
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
-      map(value => this._filter(value))
+      map(value => res)
     );
   }
 
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+  ngOnInit() {
+    this.search();
+    this.name='Login Microsoft';
   }
 
   onSearchButtonClick(){
     console.log(this.searchContent);
+    this.name = this.name + '1';
+    this.search();
+  }
+
+  onLogin(){
+
   }
 }
 
